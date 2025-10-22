b6bbd521a0122e11223a8de38375f9884efe7be9
John McDonald <mcdona22@gmail.com>
2025-10-22 16:38:46 +0100

    failin test included

--- apps/match-hub-ui/src/app/app.spec.ts
+++ apps/match-hub-ui/src/app/app.spec.ts
@@ -11,13 +11,6 @@ describe('App', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, match-hub-ui');
+    expect(app).withContext('deliberate fail').toBeFalsy();
   });
 });
