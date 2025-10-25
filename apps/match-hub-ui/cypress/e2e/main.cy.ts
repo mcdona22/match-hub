d39761f3f32c7e3e6c6a7c5235ab254a81bc80d3
John McDonald <mcdona22@gmail.com>
2025-10-25 12:04:24 +0100

    tests to check responsive behaviour

--- apps/match-hub-ui/cypress/e2e/main.cy.ts
+++ apps/match-hub-ui/cypress/e2e/main.cy.ts
@@ -25,4 +25,22 @@ describe('Main App Page', () => {
       });
     }
   });
+
+  describe('responsive tests', () => {
+    const VIEW_PORT_HEIGHT = 900;
+    const BREAKPOINT_MOBILE_MAX = 767;
+    const BREAKPOINT_DESKTOP_MIN = 768;
+
+    it('should show the menu icon and hide nav links in narrow screens', () => {
+      cy.viewport(BREAKPOINT_MOBILE_MAX, VIEW_PORT_HEIGHT);
+      cy.get('.menu-items > a').should('not.be.visible');
+      cy.get('.menu-items > button[mat-icon-button]').should('be.visible');
+    });
+
+    it('should hide the menu icon and hide nav links in larger  screens', () => {
+      cy.viewport(BREAKPOINT_DESKTOP_MIN, VIEW_PORT_HEIGHT);
+      cy.get('.menu-items > a').should('be.visible');
+      cy.get('.menu-items > button[mat-icon-button]').should('not.be.visible');
+    });
+  });
 });
